db8c0b8d8a09922f0333e781789d3c357eaf4a82
Peter Johnson <pjohnson@cluricon.com>
2017-02-07 00:57:44 -0500

    Initial commit

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
-# s3_file_manager
+# S3 File Manager
+There are probably a log of S3 File Managers out there but this one is mine.
+It seemed like a fun project and so here it is. Not much to it at the moment
+but it'll get there, probably.
---
+++ config/webpack.config.js
@@ -0,0 +1,20 @@
+module.exports = {
+    entry: './src/main.jsx',
+    output: { filename: './public/build/bundle.js' },
+    module: {
+        loaders: [
+            {
+                test: /\.jsx?$/,
+                loader: 'babel-loader',
+                exclude: /node_modules/,
+                query: {
+                    presets: ['es2015', 'react']
+                }
+            },
+            {
+                test: /\.css$/,
+                loader: 'style-loader!css-loader'
+            }
+        ]
+    },
+};
